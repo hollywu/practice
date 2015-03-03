cfe7c26db96125da5141c94e7cd177bb63d1308a
holly <holly@localhost>
2015-03-03 11:42:09 -0500

    added math functions

--- practice.php
+++ practice.php
@@ -1,6 +1,14 @@
 <?php
-// I HAVE CHANGED THIS
-//merge conflicts?
-//adasdasd
-
-//trarasdaldkjaldja
+class doMath {
+    public function add ($number1, $number2) {
+        return $number1 + $number2;
+    }
+    
+    public function subtract ($number1, $number2) {
+        return $number1 - $number2;
+    }
+    
+    public function multiply ($number1, $number2) {
+        return $number1 * $number2;
+    }
+}
