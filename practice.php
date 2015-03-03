c290b74bbb0210cd1781ae1e1331e77fcc7aeb8b
holly <holly@localhost>
2015-03-03 11:46:08 -0500

    added construct

--- practice.php
+++ practice.php
@@ -1,5 +1,13 @@
 <?php
 class doMath {
+    private $num1,
+            $num2;
+    
+    public function __construct($number1, $number2) {
+        $this->num1 = $number1;
+        $this->num2 = $number2;
+    }
+    
     public function add ($number1, $number2) {
         return $number1 + $number2;
     }
